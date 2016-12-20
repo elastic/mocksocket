bae85d2fa8144d7d6edc80c25f4b9ec8609925d3
Tim Brooks <tim@uncontended.net>
2016-12-20 11:13:45 -0600

    Add MockSocket that wraps connect and bind in doprivileged blocks

---
+++ src/main/java/org/elasticsearch/MockSocket.java
@@ -0,0 +1,81 @@
+/*
+ * Licensed to Elasticsearch under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.elasticsearch;
+
+import java.io.IOException;
+import java.net.*;
+import java.security.AccessController;
+import java.security.PrivilegedActionException;
+import java.security.PrivilegedExceptionAction;
+
+public class MockSocket extends Socket {
+
+    public MockSocket() {
+    }
+
+    public MockSocket(Proxy proxy) {
+        super(proxy);
+    }
+
+    public MockSocket(String host, int port) throws IOException {
+        super(host, port);
+    }
+
+    public MockSocket(InetAddress address, int port) throws IOException {
+        super(address, port);
+    }
+
+    public MockSocket(String host, int port, InetAddress localAddr, int localPort) throws IOException {
+        super(host, port, localAddr, localPort);
+    }
+
+    public MockSocket(InetAddress address, int port, InetAddress localAddr, int localPort) throws IOException {
+        super(address, port, localAddr, localPort);
+    }
+
+    @Override
+    public void connect(SocketAddress endpoint) throws IOException {
+        connect(endpoint, 0);
+    }
+
+    @Override
+    public void connect(SocketAddress endpoint, int timeout) throws IOException {
+        try {
+            AccessController.doPrivileged((PrivilegedExceptionAction<Void>) () -> {
+                MockSocket.super.connect(endpoint, timeout);
+                return null;
+            });
+        } catch (PrivilegedActionException e) {
+            throw (IOException) e.getCause();
+        }
+    }
+
+    @Override
+    public void bind(SocketAddress bindpoint) throws IOException {
+        try {
+            AccessController.doPrivileged((PrivilegedExceptionAction<Void>) () -> {
+                MockSocket.super.bind(bindpoint);
+                return null;
+            });
+        } catch (PrivilegedActionException e) {
+            throw (IOException) e.getCause();
+        }
+    }
+}
